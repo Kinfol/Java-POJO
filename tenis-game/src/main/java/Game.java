5c16a16b658275a68ff3e183b9c28ef8bb38614c
KinFol <KinFol>
2020-03-23 14:16:25 +0000

    add complexity

--- tenis-game/src/main/java/Game.java
+++ tenis-game/src/main/java/Game.java
@@ -2,16 +2,18 @@ public class Game {
     String player1;
     String player2;
 
+    public Game() {}
+
+    public Game(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
     public String getScore() {
-        if (noPlayerNames()) {
-            return "Love-all";
+        if (player1 == null && player2 == null) {
+            return "Love all";
         }
 
-        return String.format("%s: Love, %s Love", player1, player2);
+        return String.format("%s: Love, %s: Love", player1, player2);
     }
-
-    private boolean noPlayerNames() {
-        return player1 == null && player2 == null;
-    }
-
 }
--- tenis-game/src/test/java/GameLogicTest.java
+++ tenis-game/src/test/java/GameLogicTest.java
@@ -5,8 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TestGame {
 
     @Test
-    public void testSetInitialScoreLoveAll() {
-        Game game = new Game();
-        assertEquals("Love-all", game.getScore());
+    public void score_starts_at_love_and_includes_the_players_names() {
+        Game game = new Game("Bob", "Fred");
+
+        assertEquals("Bob: Love, Fred: Love", game.getScore());
     }
 }
